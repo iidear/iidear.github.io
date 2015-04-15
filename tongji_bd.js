34f2d916e9a3ed63c5004d67299ca4a243af7c60
iidear <iidear2015@gmail.com>
2015-04-15 21:49:17 +0800

    直接嵌入百度站长统计代码

--- tongji_bd.js
+++
@@ -1,9 +0,0 @@
-<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?ffbe20012ef4cb34b0eae3f55ba51a50";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>
