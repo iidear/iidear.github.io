10fdf6ca239e145932865d51ee9c972026b500d4
iidear <iidear2015@gmail.com>
2015-04-15 21:42:48 +0800

    baidu zhanzhang tongji

---
+++ tongji_bd.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?ffbe20012ef4cb34b0eae3f55ba51a50";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
